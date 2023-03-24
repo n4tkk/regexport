388e7aa3aa44fd8ee7bcd930944fa4ef107d3c2d
n4tkk <natsuki@kirigakure.net>
2023-03-24 16:37:59 +0900

    WIP

---
+++ cmd/regexport.go
@@ -0,0 +1,35 @@
+package cmd
+
+import (
+	"encoding/csv"
+	"os"
+	"regexp"
+)
+
+func check(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func Regex(pattern, input, output string) {
+	data, err := os.ReadFile(input)
+	check(err)
+
+	file, err := os.Create(output)
+	check(err)
+	w := csv.NewWriter(file)
+	defer w.Flush()
+
+	r := regexp.MustCompile(pattern)
+	ms := r.FindAllSubmatch(data, -1)
+
+	for _, m := range ms {
+		var row []string
+		for _, g := range m {
+			row = append(row, string(g))
+		}
+		err := w.Write(row)
+		check(err)
+	}
+}
